da736cff56333d8215fe89b5429bfa4dbb3e102a
asdaa123@126.com <xutong@123>
2018-01-23 20:31:37 +0800

    添加oracle数据源

---
+++ src/main/java/com/company/project/common/listener/ApplicationContextListener.java
@@ -0,0 +1,23 @@
+package com.company.project.common.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * spring容器初始化完成事件
+ */
+public class ApplicationContextListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationContextListener.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+    }
+
+}
---
+++ src/main/java/com/company/project/configurer/DruidDataSourceConfig.java
@@ -0,0 +1,95 @@
+package com.company.project.configurer;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class DruidDataSourceConfig  {
+    private Logger logger=Logger.getLogger(DruidDataSourceConfig.class);
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("{spring.datasource.connectionProperties}")
+    private String connectionProperties;
+    @Bean
+    DataSource dataSource() throws SQLException {
+        DruidDataSource datasource = new DruidDataSource();
+
+        datasource.setUrl(this.url);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+//configuration
+//        datasource.setInitialSize(initialSize);
+//        datasource.setMinIdle(minIdle);
+//        datasource.setMaxActive(maxActive);
+//        datasource.setMaxWait(maxWait);
+//        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+//        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+//        datasource.setValidationQuery(validationQuery);
+//        datasource.setTestWhileIdle(testWhileIdle);
+//        datasource.setTestOnBorrow(testOnBorrow);
+//        datasource.setTestOnReturn(testOnReturn);
+//        datasource.setPoolPreparedStatements(poolPreparedStatements);
+//        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+//        try {
+//            datasource.setFilters(filters);
+//        } catch (SQLException e) {
+//            logger.error("druid configuration initialization filter", e);
+//        }
+//        datasource.setConnectionProperties(connectionProperties);
+        return datasource;
+    }
+
+
+}
